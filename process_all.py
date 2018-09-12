2fd3d53b56170af72f6bea39cda518edb48b8465
Aiden Nibali <dismaldenizen@gmail.com>
2018-09-12 14:45:31 +1000

    Process all subactions

--- process_all.py
+++ process_all.py
@@ -7,11 +7,15 @@ import numpy as np
 import h5py
 from subprocess import call
 from tempfile import TemporaryDirectory
-from itertools import product
 from tqdm import tqdm
 
+from metadata import load_h36m_metadata
 
-subjects = {
+
+metadata = load_h36m_metadata()
+
+# Subjects to include when preprocessing
+included_subjects = {
     'S1': 1,
     'S5': 5,
     'S6': 6,
@@ -20,35 +24,15 @@ subjects = {
     'S9': 9,
     'S11': 11,
 }
-actions = {
-    'Directions': 1,
-    'Discussion': 2,
-    'Eating': 3,
-    'Greeting': 4,
-    'Phoning': 5,
-    'Posing': 6,
-    'Purchases': 7,
-    'Sitting': 8,
-    'SittingDown': 9,
-    'Smoking': 10,
-    'TakingPhoto': 11,
-    'Waiting': 12,
-    'Walking': 13,
-    'WalkingDog': 14,
-    'WalkTogether': 15,
-}
-cameras = {
-    '54138969': 0,
-    '55011271': 1,
-    '58860488': 2,
-    '60457274': 3,
+
+# Sequences with known issues
+blacklist = {
+    ('S11', '2', '2', '54138969'),  # Video file is corrupted
 }
 
 
-class MissingDataException(Exception):
-    pass
-
-
+# Rather than include every frame from every video, we can instead wait for the pose to change
+# significantly before storing a new example.
 def select_frame_indices_to_include(subject, poses_3d_univ):
     # To process every single frame, uncomment the following line:
     # return np.arange(0, len(poses_3d_univ))
@@ -73,6 +57,7 @@ def select_frame_indices_to_include(subject, poses_3d_univ):
 
 
 def infer_camera_intrinsics(points2d, points3d):
+    """Infer camera instrinsics from 2D<->3D point correspondences."""
     pose2d = points2d.reshape(-1, 2)
     pose3d = points3d.reshape(-1, 3)
     x3d = np.stack([pose3d[:, 0], pose3d[:, 2]], axis=-1)
@@ -84,31 +69,19 @@ def infer_camera_intrinsics(points2d, points3d):
     return np.array([alpha_x, x_0, alpha_y, y_0])
 
 
-def process_view(subject, action, camera):
+def process_view(out_dir, subject, action, subaction, camera):
     subj_dir = path.join('extracted', subject)
-    act_cam = '.'.join([action, camera])
 
-    file_found = False
-    for i in range(1, 100):
-        if path.isfile(path.join(subj_dir, 'Poses_D2_Positions', act_cam + '.cdf')):
-            file_found = True
-            break
-        act_cam = '{} {:d}.{}'.format(action, i, camera)
+    base_filename = metadata.get_base_filename(subject, action, subaction, camera)
 
-    # Workaround for corrupt video file
-    if subject == 'S11' and action == 'Directions' and camera == '54138969':
-        act_cam = 'Directions 1.54138969'
-
-    if not file_found:
-        raise MissingDataException('missing data for {}/{}.{}'.format(subject, action, camera))
-
-    with pycdf.CDF(path.join(subj_dir, 'Poses_D2_Positions', act_cam + '.cdf')) as cdf:
+    # Load joint position annotations
+    with pycdf.CDF(path.join(subj_dir, 'Poses_D2_Positions', base_filename + '.cdf')) as cdf:
         poses_2d = np.array(cdf['Pose'])
         poses_2d = poses_2d.reshape(poses_2d.shape[1], 32, 2)
-    with pycdf.CDF(path.join(subj_dir, 'Poses_D3_Positions_mono_universal', act_cam + '.cdf')) as cdf:
+    with pycdf.CDF(path.join(subj_dir, 'Poses_D3_Positions_mono_universal', base_filename + '.cdf')) as cdf:
         poses_3d_univ = np.array(cdf['Pose'])
         poses_3d_univ = poses_3d_univ.reshape(poses_3d_univ.shape[1], 32, 3)
-    with pycdf.CDF(path.join(subj_dir, 'Poses_D3_Positions_mono', act_cam + '.cdf')) as cdf:
+    with pycdf.CDF(path.join(subj_dir, 'Poses_D3_Positions_mono', base_filename + '.cdf')) as cdf:
         poses_3d = np.array(cdf['Pose'])
         poses_3d = poses_3d.reshape(poses_3d.shape[1], 32, 3)
 
@@ -118,20 +91,22 @@ def process_view(subject, action, camera):
 
     frame_indices = select_frame_indices_to_include(subject, poses_3d_univ)
     frames = frame_indices + 1
-    video_file = path.join(subj_dir, 'Videos', act_cam + '.mp4')
-    frames_dir = path.join('processed', subject, action, 'imageSequence', camera)
+    video_file = path.join(subj_dir, 'Videos', base_filename + '.mp4')
+    frames_dir = path.join(out_dir, 'imageSequence', camera)
     makedirs(frames_dir, exist_ok=True)
 
+    # Check to see whether the frame images have already been extracted previously
     existing_files = {f for f in listdir(frames_dir)}
-    skip = True
+    frames_are_extracted = True
     for i in frames:
         filename = 'img_%06d.jpg' % i
         if filename not in existing_files:
-            skip = False
+            frames_are_extracted = False
             break
 
-    if not skip:
+    if not frames_are_extracted:
         with TemporaryDirectory() as tmp_dir:
+            # Use ffmpeg to extract frames into a temporary directory
             call([
                 'ffmpeg',
                 '-nostats', '-loglevel', '0',
@@ -140,6 +115,7 @@ def process_view(subject, action, camera):
                 path.join(tmp_dir, 'img_%06d.jpg')
             ])
 
+            # Move included frame images into the output directory
             for i in frames:
                 filename = 'img_%06d.jpg' % i
                 move(
@@ -155,20 +131,28 @@ def process_view(subject, action, camera):
         'intrinsics-univ/' + camera: camera_int_univ,
         'frame': frames,
         'camera': np.full(frames.shape, int(camera)),
-        'subject': np.full(frames.shape, subjects[subject]),
-        'action': np.full(frames.shape, actions[action]),
+        'subject': np.full(frames.shape, int(included_subjects[subject])),
+        'action': np.full(frames.shape, int(action)),
+        'subaction': np.full(frames.shape, int(subaction)),
     }
 
 
-def process_sequence(subject, action):
+def process_subaction(subject, action, subaction):
     datasets = {}
 
-    for camera in tqdm(list(sorted(cameras.keys())), ascii=True, leave=False):
-        try:
-            annots = process_view(subject, action, camera)
-        except MissingDataException as ex:
-            print(str(ex))
+    out_dir = path.join('processed', subject, metadata.action_names[action] + '-' + subaction)
+    makedirs(out_dir, exist_ok=True)
+
+    for camera in tqdm(metadata.camera_ids, ascii=True, leave=False):
+        if (subject, action, subaction, camera) in blacklist:
             continue
+
+        try:
+            annots = process_view(out_dir, subject, action, subaction, camera)
+        except:
+            print('Error processing sequence, skipping: ', repr((subject, action, subaction, camera)))
+            continue
+
         for k, v in annots.items():
             if k in datasets:
                 datasets[k].append(v)
@@ -180,16 +164,25 @@ def process_sequence(subject, action):
 
     datasets = {k: np.concatenate(v) for k, v in datasets.items()}
 
-    out_dir = path.join('processed', subject, action)
-    makedirs(out_dir, exist_ok=True)
     with h5py.File(path.join(out_dir, 'annot.h5'), 'w') as f:
         for name, data in datasets.items():
             f.create_dataset(name, data=data)
 
 
 def process_all():
-    for subject, action in tqdm(list(product(subjects.keys(), actions.keys())), ascii=True, leave=False):
-        process_sequence(subject, action)
+    sequence_mappings = metadata.sequence_mappings
+
+    subactions = []
+
+    for subject in included_subjects.keys():
+        subactions += [
+            (subject, action, subaction)
+            for action, subaction in sequence_mappings[subject].keys()
+            if int(action) > 1  # Exclude '_ALL'
+        ]
+
+    for subject, action, subaction in tqdm(subactions, ascii=True, leave=False):
+        process_subaction(subject, action, subaction)
 
 
 if __name__ == '__main__':
